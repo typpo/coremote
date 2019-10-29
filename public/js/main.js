a7977fd345876c09131d5a0b968db2ecfbfd2387
Ian Webster <ianw_github@ianww.com>
2019-10-29 08:57:19 -0700

    First example

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,35 @@
-alert('hello');
+function captureImage() {
+  var scale = 1.0;
+  var video = document.querySelector('video');
+  var canvas = document.createElement('canvas');
+  canvas.width = video.videoWidth * scale;
+  canvas.height = video.videoHeight * scale;
+  canvas.getContext('2d')
+    .drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  var img = document.getElementById('me');
+  img.src = canvas.toDataURL();
+}
+
+navigator.mediaDevices.getUserMedia({ video: true })
+.then(function(mediaStream) {
+  /* use the stream */
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+    captureImage();
+    var count = 0;
+    setInterval(function() {
+      count += 100;
+      document.querySelector('#me-container .progress-image').style.width = (count / 10000 * 100) + '%';
+      if (count === 10000) {
+        captureImage();
+        count = 0;
+      }
+    }, 100);
+  };
+})
+.catch(function(err) {
+  console.error(err);
+});
